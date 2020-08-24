cbf189f1a72082730b48f0ff5a855e9afbb38f93
brokenhoax <brokenhoax@outlook.com>
2020-08-24 01:13:58 -0500

    Updates

---
+++ precache-manifest.cbb75f4b59b3e7dd4d9d162b698cb94a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1035b310fc3c87f70239bd526d63598f",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/index.html"
+  },
+  {
+    "revision": "d7ed3d940d62ac04ae55",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "cedb2d65bc4a5512ed39",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/css/main.fa45cd6b.chunk.css"
+  },
+  {
+    "revision": "d7ed3d940d62ac04ae55",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/js/2.8ed74aff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/js/2.8ed74aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedb2d65bc4a5512ed39",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/js/main.f8792616.chunk.js"
+  },
+  {
+    "revision": "c7fa45fe37e6cd3c43f1",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/js/runtime-main.d17ff1b5.js"
+  },
+  {
+    "revision": "f72b01490b90fdbb40732dffffa3d474",
+    "url": "/Kraus_Homework_React_Portfolio_8-22-2020/static/media/codesplay.f72b0149.jpg"
+  }
+]);
